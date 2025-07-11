d22b57a944b2245668209e1e67f9594cd11809f8
bacf <brunoagustincarusofassa@gmail.com>
2025-07-11 11:36:14 +0200

    Initial commit

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FactAttack
+//
+//  Created by Bruno Agustín Caruso Fassa on 10/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FactAttackApp.swift
@@ -0,0 +1,17 @@
+//
+//  FactAttackApp.swift
+//  FactAttack
+//
+//  Created by Bruno Agustín Caruso Fassa on 10/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct FactAttackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
