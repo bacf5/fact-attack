d5779067da26c43b800476e93faa868b172b23ce
bacf <brunoagustincarusofassa@gmail.com>
2025-07-16 21:14:57 +0200

    Created model for cats facts and dog img

---
+++ CatsFact.swift
@@ -0,0 +1,10 @@
+//
+//  CatsFact.swift
+//  FactAttack
+//
+//  Created by Bruno Agustín Caruso Fassa on 16/07/2025.
+//
+
+struct CatsFact: Decodable {
+    let data: [Facts]
+}
---
+++ Dogs.swift
@@ -0,0 +1,14 @@
+//
+//  Dogs.swift
+//  FactAttack
+//
+//  Created by Bruno Agustín Caruso Fassa on 16/07/2025.
+//
+
+struct Dogs: Decodable {
+    let message: String
+}
+
+
+
+
